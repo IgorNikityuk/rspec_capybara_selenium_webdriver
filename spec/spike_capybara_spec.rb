b09b617c748f1ae1482599a43cd468a6d479d225
Igor <Igor.nikityuk@gmail.com>
2014-05-07 16:51:04 +0300

    Update spike_capybara_spec.rb

--- spec/spike_capybara_spec.rb
+++ spec/spike_capybara_spec.rb
@@ -11,7 +11,7 @@ describe "Searching for a video" do
   it "allows searches for general terms" do
     visit ""
 	fill_in('search_query', :with => "text adventures")
-	click_button('se1arch-btn')
+	click_button('search-btn')
 	page.should have_content("GET LAMP: The Text Adventure Documentary")
   end
 end

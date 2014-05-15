731247fae5649e5702362aab485b2f821ac932fc
Igor <igor.nikityuk@gmail.com>
2014-05-15 16:15:42 +0300

    add support fiolder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,16 @@ require 'capybara'
 require 'capybara/dsl'
 require 'selenium-webdriver'
 
+#require files
+require 'support/login_page.rb'
+
+#Dir["support/*.rb"].each {|file| require file}
+
 RSpec.configure do |config|
-	config.include Capybara::DSL
+  config.include Capybara::DSL
+
 end
 
 Capybara.default_driver = :selenium
 Capybara.app_host = "http://www.youtube.com"
+Capybara.default_host = 'http://staging.limos.com/'
